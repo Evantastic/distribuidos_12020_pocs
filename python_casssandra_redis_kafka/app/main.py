3acdef5ffec87200a225f014cd6b372ac63033b3
Alan Barahona Ruiz <barahonaruiz.alan@gmail.com>
2020-07-18 15:09:16 -0400

    Kafka library changed
    Now it uses confluent-kafka instead of kafka-python

--- python_casssandra_redis_kafka/app/main.py
+++ python_casssandra_redis_kafka/app/main.py
@@ -2,7 +2,7 @@ from redis import Redis
 from cassandra.cluster import Cluster
 from os import getenv
 from sys import stdout
-from kafka import KafkaConsumer
+from confluent_kafka import Consumer
 
 # Redis
 redis_host = getenv('REDIS_HOST')
@@ -17,8 +17,23 @@ cluster = Cluster(['cassandra'])
 session = cluster.connect()
 
 # Kafka
-consumer = KafkaConsumer('test', bootstrap_servers='kafka')
-for msg in consumer:
-    print(msg)
-    # Esto es para que imprima, o si no no funciona
+c = Consumer({
+    'bootstrap.servers': 'kafka',
+    'group.id': 'mygroup',
+    'auto.offset.reset': 'beginning'
+})
+
+c.subscribe(['test'])
+
+print("Ready to receive messages")
+while True:
     stdout.flush()
+    msg = c.poll(1.0)
+    if msg is None:
+        continue
+    if msg.error():
+        print("Consumer error: {}".format(msg.error()))
+        continue
+    print('Received message: {}'.format(msg.value().decode('utf-8')))
+
+c.close()
