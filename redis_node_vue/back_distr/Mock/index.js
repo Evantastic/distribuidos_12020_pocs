9cda9bceaefcb3fead4623c0ccf953fc650c4634
hackasteroid142 <vicente.vega@usach.cl>
2020-07-21 18:15:57 -0400

    Change in database

--- redis_node_vue/back_distr/Mock/index.js
+++ redis_node_vue/back_distr/Mock/index.js
@@ -5,17 +5,65 @@ const redisClient = redis.createClient({
     port: process.env.REDISPORT
 });
 
-redisClient.hmset('1',{nombre: 'Planeta 1', x: '320', y: '250', promedio:'100', error: '150', desviacion:'310'});
-redisClient.hmset('2',{nombre: 'Planeta 2', x: '350', y: '350', promedio:'200', error: '160', desviacion:'320'});
-redisClient.hmset('3',{nombre: 'Planeta 3', x: '370', y: '450', promedio:'300', error: '170', desviacion:'330'});
-redisClient.hmset('4',{nombre: 'Planeta 4', x: '390', y: '550', promedio:'400', error: '180', desviacion:'340'});
-redisClient.hmset('5',{nombre: 'Planeta 5', x: '410', y: '650', promedio:'500', error: '190', desviacion:'350'});
-redisClient.hmset('6',{nombre: 'Planeta 6', x: '430', y: '750', promedio:'600', error: '200', desviacion:'360'});
-redisClient.hmset('7',{nombre: 'Planeta 7', x: '450', y: '850', promedio:'700', error: '210', desviacion:'370'});
-redisClient.hmset('8',{nombre: 'Planeta 8', x: '470', y: '950', promedio:'800', error: '220', desviacion:'380'});
-redisClient.hmset('9',{nombre: 'Planeta 9', x: '490', y: '1050', promedio:'900', error: '230', desviacion:'390'});
-redisClient.hmset('10',{nombre: 'Planeta 10', x: '510', y: '1150', promedio:'1000', error: '240', desviacion:'400'});
-redisClient.hmset('11',{nombre: 'Planeta 11', x: '520', y: '1250', promedio:'1100', error: '250', desviacion:'410'});
-redisClient.hmset('12',{nombre: 'Planeta 12', x: '550', y: '1350', promedio:'1200', error: '260', desviacion:'420'});
+redisClient.hmset('1',{nombre: 'Planeta 1', 
+            magpsf:'100',magpsf_prom:'200',magpsf_min:'300',magpsf_max:'500',magpsf_med:'600',magpsf_std:'700',magpsf_delta:'900',
+            sigmapsf:'200',sigmapsf_prom:'500',sigmapsf_min:'500',sigmapsf_max:'700',sigmapsf_med:'800',sigmapsf_std:'500',sigmapsf_std:'900',sigmapsf_delta:'800',
+            magpsf_corr:'300',magpsf_promCorr:'400',magpsf_minCorr:'700',magpsf_maxCorr:'800',magpsf_medCorr:'900',magpsf_stdCorr:'200',magpsf_deltaCorr:'1200',
+            sigmapsf_corr:'400',sigmapsf_promCorr:'800',sigmapsf_minCorr:'500',sigmapsf_maxCorr:'900',sigmapsf_medCorr:'1500',sigmapsf_stdCorr:'1600',sigmapsf_deltaCorr:'1700'});
+redisClient.hmset('2',{nombre: 'Planeta 2', 
+            magpsf:'100',magpsf_prom:'200',magpsf_min:'300',magpsf_max:'500',magpsf_med:'600',magpsf_std:'700',magpsf_delta:'900',
+            sigmapsf:'200',sigmapsf_prom:'500',sigmapsf_min:'500',sigmapsf_max:'700',sigmapsf_med:'800',sigmapsf_std:'500',sigmapsf_std:'900',sigmapsf_delta:'800',
+            magpsf_corr:'300',magpsf_promCorr:'400',magpsf_minCorr:'700',magpsf_maxCorr:'800',magpsf_medCorr:'900',magpsf_stdCorr:'200',magpsf_deltaCorr:'1200',
+            sigmapsf_corr:'400',sigmapsf_promCorr:'800',sigmapsf_minCorr:'500',sigmapsf_maxCorr:'900',sigmapsf_medCorr:'1500',sigmapsf_stdCorr:'1600',sigmapsf_deltaCorr:'1700'});
+redisClient.hmset('3',{nombre: 'Planeta 3',
+            magpsf:'100',magpsf_prom:'200',magpsf_min:'300',magpsf_max:'500',magpsf_med:'600',magpsf_std:'700',magpsf_delta:'900',
+            sigmapsf:'200',sigmapsf_prom:'500',sigmapsf_min:'500',sigmapsf_max:'700',sigmapsf_med:'800',sigmapsf_std:'500',sigmapsf_std:'900',sigmapsf_delta:'800',
+            magpsf_corr:'300',magpsf_promCorr:'400',magpsf_minCorr:'700',magpsf_maxCorr:'800',magpsf_medCorr:'900',magpsf_stdCorr:'200',magpsf_deltaCorr:'1200',
+            sigmapsf_corr:'400',sigmapsf_promCorr:'800',sigmapsf_minCorr:'500',sigmapsf_maxCorr:'900',sigmapsf_medCorr:'1500',sigmapsf_stdCorr:'1600',sigmapsf_deltaCorr:'1700'});
+redisClient.hmset('4',{nombre: 'Planeta 4', 
+            magpsf:'100',magpsf_prom:'200',magpsf_min:'300',magpsf_max:'500',magpsf_med:'600',magpsf_std:'700',magpsf_delta:'900',
+            sigmapsf:'200',sigmapsf_prom:'500',sigmapsf_min:'500',sigmapsf_max:'700',sigmapsf_med:'800',sigmapsf_std:'500',sigmapsf_std:'900',sigmapsf_delta:'800',
+            magpsf_corr:'300',magpsf_promCorr:'400',magpsf_minCorr:'700',magpsf_maxCorr:'800',magpsf_medCorr:'900',magpsf_stdCorr:'200',magpsf_deltaCorr:'1200',
+            sigmapsf_corr:'400',sigmapsf_promCorr:'800',sigmapsf_minCorr:'500',sigmapsf_maxCorr:'900',sigmapsf_medCorr:'1500',sigmapsf_stdCorr:'1600',sigmapsf_deltaCorr:'1700'});
+redisClient.hmset('5',{nombre: 'Planeta 5',
+            magpsf:'100',magpsf_prom:'200',magpsf_min:'300',magpsf_max:'500',magpsf_med:'600',magpsf_std:'700',magpsf_delta:'900',
+            sigmapsf:'200',sigmapsf_prom:'500',sigmapsf_min:'500',sigmapsf_max:'700',sigmapsf_med:'800',sigmapsf_std:'500',sigmapsf_std:'900',sigmapsf_delta:'800',
+            magpsf_corr:'300',magpsf_promCorr:'400',magpsf_minCorr:'700',magpsf_maxCorr:'800',magpsf_medCorr:'900',magpsf_stdCorr:'200',magpsf_deltaCorr:'1200',
+            sigmapsf_corr:'400',sigmapsf_promCorr:'800',sigmapsf_minCorr:'500',sigmapsf_maxCorr:'900',sigmapsf_medCorr:'1500',sigmapsf_stdCorr:'1600',sigmapsf_deltaCorr:'1700'});
+redisClient.hmset('6',{nombre: 'Planeta 6', 
+            magpsf:'100',magpsf_prom:'200',magpsf_min:'300',magpsf_max:'500',magpsf_med:'600',magpsf_std:'700',magpsf_delta:'900',
+            sigmapsf:'200',sigmapsf_prom:'500',sigmapsf_min:'500',sigmapsf_max:'700',sigmapsf_med:'800',sigmapsf_std:'500',sigmapsf_std:'900',sigmapsf_delta:'800',
+            magpsf_corr:'300',magpsf_promCorr:'400',magpsf_minCorr:'700',magpsf_maxCorr:'800',magpsf_medCorr:'900',magpsf_stdCorr:'200',magpsf_deltaCorr:'1200',
+            sigmapsf_corr:'400',sigmapsf_promCorr:'800',sigmapsf_minCorr:'500',sigmapsf_maxCorr:'900',sigmapsf_medCorr:'1500',sigmapsf_stdCorr:'1600',sigmapsf_deltaCorr:'1700'});
+redisClient.hmset('7',{nombre: 'Planeta 7', 
+            magpsf:'100',magpsf_prom:'200',magpsf_min:'300',magpsf_max:'500',magpsf_med:'600',magpsf_std:'700',magpsf_delta:'900',
+            sigmapsf:'200',sigmapsf_prom:'500',sigmapsf_min:'500',sigmapsf_max:'700',sigmapsf_med:'800',sigmapsf_std:'500',sigmapsf_std:'900',sigmapsf_delta:'800',
+            magpsf_corr:'300',magpsf_promCorr:'400',magpsf_minCorr:'700',magpsf_maxCorr:'800',magpsf_medCorr:'900',magpsf_stdCorr:'200',magpsf_deltaCorr:'1200',
+            sigmapsf_corr:'400',sigmapsf_promCorr:'800',sigmapsf_minCorr:'500',sigmapsf_maxCorr:'900',sigmapsf_medCorr:'1500',sigmapsf_stdCorr:'1600',sigmapsf_deltaCorr:'1700'});
+redisClient.hmset('8',{nombre: 'Planeta 8',
+            magpsf:'100',magpsf_prom:'200',magpsf_min:'300',magpsf_max:'500',magpsf_med:'600',magpsf_std:'700',magpsf_delta:'900',
+            sigmapsf:'200',sigmapsf_prom:'500',sigmapsf_min:'500',sigmapsf_max:'700',sigmapsf_med:'800',sigmapsf_std:'500',sigmapsf_std:'900',sigmapsf_delta:'800',
+            magpsf_corr:'300',magpsf_promCorr:'400',magpsf_minCorr:'700',magpsf_maxCorr:'800',magpsf_medCorr:'900',magpsf_stdCorr:'200',magpsf_deltaCorr:'1200',
+            sigmapsf_corr:'400',sigmapsf_promCorr:'800',sigmapsf_minCorr:'500',sigmapsf_maxCorr:'900',sigmapsf_medCorr:'1500',sigmapsf_stdCorr:'1600',sigmapsf_deltaCorr:'1700'});
+redisClient.hmset('9',{nombre: 'Planeta 9', 
+            magpsf:'100',magpsf_prom:'200',magpsf_min:'300',magpsf_max:'500',magpsf_med:'600',magpsf_std:'700',magpsf_delta:'900',
+            sigmapsf:'200',sigmapsf_prom:'500',sigmapsf_min:'500',sigmapsf_max:'700',sigmapsf_med:'800',sigmapsf_std:'500',sigmapsf_std:'900',sigmapsf_delta:'800',
+            magpsf_corr:'300',magpsf_promCorr:'400',magpsf_minCorr:'700',magpsf_maxCorr:'800',magpsf_medCorr:'900',magpsf_stdCorr:'200',magpsf_deltaCorr:'1200',
+            sigmapsf_corr:'400',sigmapsf_promCorr:'800',sigmapsf_minCorr:'500',sigmapsf_maxCorr:'900',sigmapsf_medCorr:'1500',sigmapsf_stdCorr:'1600',sigmapsf_deltaCorr:'1700'});
+redisClient.hmset('10',{nombre: 'Planeta 10', 
+            magpsf:'100',magpsf_prom:'200',magpsf_min:'300',magpsf_max:'500',magpsf_med:'600',magpsf_std:'700',magpsf_delta:'900',
+            sigmapsf:'200',sigmapsf_prom:'500',sigmapsf_min:'500',sigmapsf_max:'700',sigmapsf_med:'800',sigmapsf_std:'500',sigmapsf_std:'900',sigmapsf_delta:'800',
+            magpsf_corr:'300',magpsf_promCorr:'400',magpsf_minCorr:'700',magpsf_maxCorr:'800',magpsf_medCorr:'900',magpsf_stdCorr:'200',magpsf_deltaCorr:'1200',
+            sigmapsf_corr:'400',sigmapsf_promCorr:'800',sigmapsf_minCorr:'500',sigmapsf_maxCorr:'900',sigmapsf_medCorr:'1500',sigmapsf_stdCorr:'1600',sigmapsf_deltaCorr:'1700'});
+redisClient.hmset('11',{nombre: 'Planeta 11', 
+            magpsf:'100',magpsf_prom:'200',magpsf_min:'300',magpsf_max:'500',magpsf_med:'600',magpsf_std:'700',magpsf_delta:'900',
+            sigmapsf:'200',sigmapsf_prom:'500',sigmapsf_min:'500',sigmapsf_max:'700',sigmapsf_med:'800',sigmapsf_std:'500',sigmapsf_std:'900',sigmapsf_delta:'800',
+            magpsf_corr:'300',magpsf_promCorr:'400',magpsf_minCorr:'700',magpsf_maxCorr:'800',magpsf_medCorr:'900',magpsf_stdCorr:'200',magpsf_deltaCorr:'1200',
+            sigmapsf_corr:'400',sigmapsf_promCorr:'800',sigmapsf_minCorr:'500',sigmapsf_maxCorr:'900',sigmapsf_medCorr:'1500',sigmapsf_stdCorr:'1600',sigmapsf_deltaCorr:'1700'});
+redisClient.hmset('12',{nombre: 'Planeta 12', 
+            magpsf:'100',magpsf_prom:'200',magpsf_min:'300',magpsf_max:'500',magpsf_med:'600',magpsf_std:'700',magpsf_delta:'900',
+            sigmapsf:'200',sigmapsf_prom:'500',sigmapsf_min:'500',sigmapsf_max:'700',sigmapsf_med:'800',sigmapsf_std:'500',sigmapsf_std:'900',sigmapsf_delta:'800',
+            magpsf_corr:'300',magpsf_promCorr:'400',magpsf_minCorr:'700',magpsf_maxCorr:'800',magpsf_medCorr:'900',magpsf_stdCorr:'200',magpsf_deltaCorr:'1200',
+            sigmapsf_corr:'400',sigmapsf_promCorr:'800',sigmapsf_minCorr:'500',sigmapsf_maxCorr:'900',sigmapsf_medCorr:'1500',sigmapsf_stdCorr:'1600',sigmapsf_deltaCorr:'1700'});
 
-console.log('Listo')
+console.log('Listo');
--- redis_node_vue/front/src/views/Planet.vue
+++ redis_node_vue/front/src/views/Planet.vue
@@ -19,12 +19,20 @@
                             <v-list-item-content class="align-end">{{ planet.nombre }}</v-list-item-content>
                         </v-list-item>
                         <v-list-item>
-                            <v-list-item-content>Eje x: </v-list-item-content>
-                            <v-list-item-content class="align-end">{{ planet.x }}</v-list-item-content>
+                            <v-list-item-content>Magpsf: </v-list-item-content>
+                            <v-list-item-content class="align-end">{{ planet.magpsf }}</v-list-item-content>
                         </v-list-item>
                         <v-list-item>
-                            <v-list-item-content>Eje y: </v-list-item-content>
-                            <v-list-item-content class="align-end">{{ planet.y }}</v-list-item-content>
+                            <v-list-item-content>Sigmapsf: </v-list-item-content>
+                            <v-list-item-content class="align-end">{{ planet.sigmapsf }}</v-list-item-content>
+                        </v-list-item>
+                        <v-list-item>
+                            <v-list-item-content>Magpsf Corregido: </v-list-item-content>
+                            <v-list-item-content class="align-end">{{ planet.magpsf_corr }}</v-list-item-content>
+                        </v-list-item>
+                        <v-list-item>
+                            <v-list-item-content>Sigmapsf Corregido: </v-list-item-content>
+                            <v-list-item-content class="align-end">{{ planet.sigmapsf_corr }}</v-list-item-content>
                         </v-list-item>
                     </v-list>
                 </v-card>
@@ -36,22 +44,124 @@
                     <v-card-title>Estadisticas</v-card-title>
 
                     <v-divider></v-divider>
-                    <v-list dense>
-                        <v-list-item>
-                            <v-list-item-content>Promedio: </v-list-item-content>
-                            <v-list-item-content class="align-end">{{ planet.promedio }}</v-list-item-content>
-                        </v-list-item>
-                        <v-list-item>
-                            <v-list-item-content>Error: </v-list-item-content>
-                            <v-list-item-content class="align-end">{{ planet.error }}</v-list-item-content>
-                        </v-list-item>
-                        <v-list-item>
-                            <v-list-item-content>Desviación Estandar: </v-list-item-content>
-                            <v-list-item-content class="align-end">{{ planet.desviacion }}</v-list-item-content>
-                        </v-list-item>
-                    </v-list>
+
+                    <v-row>
+                        <v-col>
+                            <v-card-title>Magpsf</v-card-title>
+                                <v-list dense>
+                                    <v-list-item>
+                                        <v-list-item-content>Promedio: </v-list-item-content>
+                                        <v-list-item-content class="align-end">{{ planet.magpsf_prom }}</v-list-item-content>
+                                    </v-list-item>
+                                    <v-list-item>
+                                        <v-list-item-content>Mínimo: </v-list-item-content>
+                                        <v-list-item-content class="align-end">{{ planet.magpsf_min }}</v-list-item-content>
+                                    </v-list-item>
+                                    <v-list-item>
+                                        <v-list-item-content>Máximo: </v-list-item-content>
+                                        <v-list-item-content class="align-end">{{ planet.magpsf_max }}</v-list-item-content>
+                                    </v-list-item>
+                                    <v-list-item>
+                                        <v-list-item-content>Mediana: </v-list-item-content>
+                                        <v-list-item-content class="align-end">{{ planet.magpsf_med }}</v-list-item-content>
+                                    </v-list-item>
+                                    <v-list-item>
+                                        <v-list-item-content>Desviación estandar: </v-list-item-content>
+                                        <v-list-item-content class="align-end">{{ planet.magpsf_std }}</v-list-item-content>
+                                    </v-list-item>
+                                    <v-list-item>
+                                        <v-list-item-content>Delta: </v-list-item-content>
+                                        <v-list-item-content class="align-end">{{ planet.magpsf_delta }}</v-list-item-content>
+                                    </v-list-item>
+                                </v-list>
+                                <v-divider></v-divider>
+                            <v-card-title>Sigmapsf</v-card-title>
+                                <v-list dense>
+                                    <v-list-item>
+                                        <v-list-item-content>Promedio: </v-list-item-content>
+                                        <v-list-item-content class="align-end">{{ planet.sigmapsf_prom }}</v-list-item-content>
+                                    </v-list-item>
+                                    <v-list-item>
+                                        <v-list-item-content>Mínimo: </v-list-item-content>
+                                        <v-list-item-content class="align-end">{{ planet.sigmapsf_min }}</v-list-item-content>
+                                    </v-list-item>
+                                    <v-list-item>
+                                        <v-list-item-content>Máximo: </v-list-item-content>
+                                        <v-list-item-content class="align-end">{{ planet.sigmapsf_max }}</v-list-item-content>
+                                    </v-list-item>
+                                    <v-list-item>
+                                        <v-list-item-content>Mediana: </v-list-item-content>
+                                        <v-list-item-content class="align-end">{{ planet.sigmapsf_max }}</v-list-item-content>
+                                    </v-list-item>
+                                    <v-list-item>
+                                        <v-list-item-content>Desviación estandar: </v-list-item-content>
+                                        <v-list-item-content class="align-end">{{ planet.sigmapsf_max }}</v-list-item-content>
+                                    </v-list-item>
+                                    <v-list-item>
+                                        <v-list-item-content>Delta: </v-list-item-content>
+                                        <v-list-item-content class="align-end">{{ planet.sigmapsf_max }}</v-list-item-content>
+                                    </v-list-item>
+                                </v-list>
+                        </v-col>
+                        <v-col>
+                            <v-card-title>Magpsf Corregido</v-card-title>
+                                <v-list dense>
+                                    <v-list-item>
+                                        <v-list-item-content>Promedio: </v-list-item-content>
+                                        <v-list-item-content class="align-end">{{ planet.magpsf_promCorr }}</v-list-item-content>
+                                    </v-list-item>
+                                    <v-list-item>
+                                        <v-list-item-content>Mínimo: </v-list-item-content>
+                                        <v-list-item-content class="align-end">{{ planet.magpsf_minCorr }}</v-list-item-content>
+                                    </v-list-item>
+                                    <v-list-item>
+                                        <v-list-item-content>Máximo: </v-list-item-content>
+                                        <v-list-item-content class="align-end">{{ planet.magpsf_maxCorr }}</v-list-item-content>
+                                    </v-list-item>
+                                    <v-list-item>
+                                        <v-list-item-content>Mediana: </v-list-item-content>
+                                        <v-list-item-content class="align-end">{{ planet.magpsf_medCorr }}</v-list-item-content>
+                                    </v-list-item>
+                                    <v-list-item>
+                                        <v-list-item-content>Desviación estandar: </v-list-item-content>
+                                        <v-list-item-content class="align-end">{{ planet.magpsf_stdCorr }}</v-list-item-content>
+                                    </v-list-item>
+                                    <v-list-item>
+                                        <v-list-item-content>Delta: </v-list-item-content>
+                                        <v-list-item-content class="align-end">{{ planet.magpsf_deltaCorr }}</v-list-item-content>
+                                    </v-list-item>
+                                </v-list>
+                                <v-divider></v-divider>
+                            <v-card-title>Sigmapsf Corregido</v-card-title>
+                                <v-list dense>
+                                    <v-list-item>
+                                        <v-list-item-content>Promedio: </v-list-item-content>
+                                        <v-list-item-content class="align-end">{{ planet.sigmapsf_promCorr }}</v-list-item-content>
+                                    </v-list-item>
+                                    <v-list-item>
+                                        <v-list-item-content>Mínimo: </v-list-item-content>
+                                        <v-list-item-content class="align-end">{{ planet.sigmapsf_minCorr }}</v-list-item-content>
+                                    </v-list-item>
+                                    <v-list-item>
+                                        <v-list-item-content>Máximo: </v-list-item-content>
+                                        <v-list-item-content class="align-end">{{ planet.sigmapsf_maxCorr }}</v-list-item-content>
+                                    </v-list-item>
+                                    <v-list-item>
+                                        <v-list-item-content>Mediana: </v-list-item-content>
+                                        <v-list-item-content class="align-end">{{ planet.sigmapsf_maxCorr }}</v-list-item-content>
+                                    </v-list-item>
+                                    <v-list-item>
+                                        <v-list-item-content>Desviación estandar: </v-list-item-content>
+                                        <v-list-item-content class="align-end">{{ planet.sigmapsf_maxCorr }}</v-list-item-content>
+                                    </v-list-item>
+                                    <v-list-item>
+                                        <v-list-item-content>Delta: </v-list-item-content>
+                                        <v-list-item-content class="align-end">{{ planet.sigmapsf_maxCorr }}</v-list-item-content>
+                                    </v-list-item>
+                                </v-list>
+                        </v-col>
+                    </v-row>
                 </v-card>
-                
             </v-col>
         </v-row>
     </v-container>
